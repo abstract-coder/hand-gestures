8bd1f6caec05c5c8addbbd745c306a1f29bb1ac2
ishita221209 <84296284+ishita221209@users.noreply.github.com>
2022-11-07 14:25:30 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -27,11 +27,39 @@ function identify_pic() {
     classifier.classify(picture_identify, got_results);
 }
 
+
 function got_results(error, results) {
     if (error) {
         console.error(error);
     }
     else {
+        
         console.log(results);
+        prediction = results[0].label;
+        agent_speak();
+        if (prediction == "Thumbs down") {
+            document.getElementById("result_name").innerHTML = prediction;
+            document.getElementById("update_emoji").innerHTML = "&#128078;";
+        }
+        if (prediction == "OK") {
+            document.getElementById("result_name").innerHTML = prediction;
+            document.getElementById("update_emoji").innerHTML = "&#128076;";
+        }
+        if (prediction == "Thumbs up") {
+            document.getElementById("result_name").innerHTML = prediction;
+            document.getElementById("update_emoji").innerHTML = "&#128077;";
+        }
+        if (prediction == "Peace") {
+            document.getElementById("result_name").innerHTML = prediction;
+            document.getElementById("update_emoji").innerHTML = "&#9996;";
+        }
     }
+}
+
+function agent_speak() {
+    var agent_speak = window.speechSynthesis;
+    speak_pred = "The hand gesture is " + prediction;
+
+    utter_data = new SpeechSynthesisUtterance(speak_pred);
+    agent_speak.speak(utter_data);
 }
